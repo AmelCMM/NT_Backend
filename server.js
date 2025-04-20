8d7dad1d0872d87d9df550894b9099c73b870d2c
Chimuka Mukwenya <162886933+ChimukaMukwenya@users.noreply.github.com>
2025-04-20 20:37:25 +0000

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
 });
-
+app.set("trust proxy", true);
 // Security middleware
 app.use(helmet());
 app.use(xss());
